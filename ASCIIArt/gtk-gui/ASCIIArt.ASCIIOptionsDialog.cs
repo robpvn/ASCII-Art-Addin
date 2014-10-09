46b54b909413c4e8589c525f768255174da28b25
Robert Nordan <rpvn@robpvn.net>
2014-10-09 21:23:27 +0200

    Some fixing of the GUI
    (Which is not so very good!)

--- ASCIIArt/gtk-gui/ASCIIArt.ASCIIOptionsDialog.cs
+++ ASCIIArt/gtk-gui/ASCIIArt.ASCIIOptionsDialog.cs
@@ -9,6 +9,7 @@ namespace ASCIIArt
 		private global::Gtk.RadioButton radioButtonBlocks;
 		private global::Gtk.HBox hbox3;
 		private global::Gtk.RadioButton radiobutton3;
+		private global::Gtk.HBox hbox4;
 		private global::Gtk.Entry entryFieldChars;
 		private global::Gtk.Label label2;
 		private global::Gtk.HBox hbox1;
@@ -77,29 +78,38 @@ namespace ASCIIArt
 			this.hbox3.Add (this.radiobutton3);
 			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox3 [this.radiobutton3]));
 			w5.Position = 0;
-			// Container child hbox3.Gtk.Box+BoxChild
+			w1.Add (this.hbox3);
+			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox3]));
+			w6.Position = 3;
+			w6.Expand = false;
+			w6.Fill = false;
+			// Container child dialog1_VBox.Gtk.Box+BoxChild
+			this.hbox4 = new global::Gtk.HBox ();
+			this.hbox4.Name = "hbox4";
+			this.hbox4.Spacing = 6;
+			// Container child hbox4.Gtk.Box+BoxChild
 			this.entryFieldChars = new global::Gtk.Entry ();
 			this.entryFieldChars.CanFocus = true;
 			this.entryFieldChars.Name = "entryFieldChars";
 			this.entryFieldChars.IsEditable = true;
 			this.entryFieldChars.InvisibleChar = '•';
-			this.hbox3.Add (this.entryFieldChars);
-			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.hbox3 [this.entryFieldChars]));
-			w6.Position = 2;
-			w1.Add (this.hbox3);
-			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox3]));
-			w7.Position = 3;
-			w7.Expand = false;
-			w7.Fill = false;
-			// Container child dialog1_VBox.Gtk.Box+BoxChild
-			this.label2 = new global::Gtk.Label ();
-			this.label2.Name = "label2";
-			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("label2");
-			w1.Add (this.label2);
-			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(w1 [this.label2]));
+			this.hbox4.Add (this.entryFieldChars);
+			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.hbox4 [this.entryFieldChars]));
+			w7.Position = 0;
+			w1.Add (this.hbox4);
+			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox4]));
 			w8.Position = 4;
 			w8.Expand = false;
 			w8.Fill = false;
+			// Container child dialog1_VBox.Gtk.Box+BoxChild
+			this.label2 = new global::Gtk.Label ();
+			this.label2.Name = "label2";
+			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("ASCII file resolution");
+			w1.Add (this.label2);
+			global::Gtk.Box.BoxChild w9 = ((global::Gtk.Box.BoxChild)(w1 [this.label2]));
+			w9.Position = 5;
+			w9.Expand = false;
+			w9.Fill = false;
 			// Container child dialog1_VBox.Gtk.Box+BoxChild
 			this.hbox1 = new global::Gtk.HBox ();
 			this.hbox1.Name = "hbox1";
@@ -109,10 +119,10 @@ namespace ASCIIArt
 			this.label3.Name = "label3";
 			this.label3.LabelProp = global::Mono.Unix.Catalog.GetString ("Horisontal cell size");
 			this.hbox1.Add (this.label3);
-			global::Gtk.Box.BoxChild w9 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.label3]));
-			w9.Position = 0;
-			w9.Expand = false;
-			w9.Fill = false;
+			global::Gtk.Box.BoxChild w10 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.label3]));
+			w10.Position = 0;
+			w10.Expand = false;
+			w10.Fill = false;
 			// Container child hbox1.Gtk.Box+BoxChild
 			this.spinButtonW = new global::Gtk.SpinButton (0, 1000, 1);
 			this.spinButtonW.CanFocus = true;
@@ -121,15 +131,15 @@ namespace ASCIIArt
 			this.spinButtonW.ClimbRate = 1;
 			this.spinButtonW.Numeric = true;
 			this.hbox1.Add (this.spinButtonW);
-			global::Gtk.Box.BoxChild w10 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.spinButtonW]));
-			w10.Position = 1;
-			w10.Expand = false;
-			w10.Fill = false;
-			w1.Add (this.hbox1);
-			global::Gtk.Box.BoxChild w11 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox1]));
-			w11.Position = 5;
+			global::Gtk.Box.BoxChild w11 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.spinButtonW]));
+			w11.Position = 2;
 			w11.Expand = false;
 			w11.Fill = false;
+			w1.Add (this.hbox1);
+			global::Gtk.Box.BoxChild w12 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox1]));
+			w12.Position = 6;
+			w12.Expand = false;
+			w12.Fill = false;
 			// Container child dialog1_VBox.Gtk.Box+BoxChild
 			this.hbox2 = new global::Gtk.HBox ();
 			this.hbox2.Name = "hbox2";
@@ -139,10 +149,10 @@ namespace ASCIIArt
 			this.label4.Name = "label4";
 			this.label4.LabelProp = global::Mono.Unix.Catalog.GetString ("Vertical cell size");
 			this.hbox2.Add (this.label4);
-			global::Gtk.Box.BoxChild w12 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.label4]));
-			w12.Position = 0;
-			w12.Expand = false;
-			w12.Fill = false;
+			global::Gtk.Box.BoxChild w13 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.label4]));
+			w13.Position = 0;
+			w13.Expand = false;
+			w13.Fill = false;
 			// Container child hbox2.Gtk.Box+BoxChild
 			this.spinButtonH = new global::Gtk.SpinButton (0, 1000, 1);
 			this.spinButtonH.CanFocus = true;
@@ -151,21 +161,21 @@ namespace ASCIIArt
 			this.spinButtonH.ClimbRate = 1;
 			this.spinButtonH.Numeric = true;
 			this.hbox2.Add (this.spinButtonH);
-			global::Gtk.Box.BoxChild w13 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.spinButtonH]));
-			w13.Position = 1;
-			w13.Expand = false;
-			w13.Fill = false;
-			w1.Add (this.hbox2);
-			global::Gtk.Box.BoxChild w14 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox2]));
-			w14.Position = 6;
+			global::Gtk.Box.BoxChild w14 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.spinButtonH]));
+			w14.Position = 2;
 			w14.Expand = false;
 			w14.Fill = false;
+			w1.Add (this.hbox2);
+			global::Gtk.Box.BoxChild w15 = ((global::Gtk.Box.BoxChild)(w1 [this.hbox2]));
+			w15.Position = 7;
+			w15.Expand = false;
+			w15.Fill = false;
 			// Internal child ASCIIArt.ASCIIOptionsDialog.ActionArea
-			global::Gtk.HButtonBox w15 = this.ActionArea;
-			w15.Name = "dialog1_ActionArea";
-			w15.Spacing = 10;
-			w15.BorderWidth = ((uint)(5));
-			w15.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
+			global::Gtk.HButtonBox w16 = this.ActionArea;
+			w16.Name = "dialog1_ActionArea";
+			w16.Spacing = 10;
+			w16.BorderWidth = ((uint)(5));
+			w16.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonCancel = new global::Gtk.Button ();
 			this.buttonCancel.CanDefault = true;
@@ -175,9 +185,9 @@ namespace ASCIIArt
 			this.buttonCancel.UseUnderline = true;
 			this.buttonCancel.Label = "gtk-cancel";
 			this.AddActionWidget (this.buttonCancel, -6);
-			global::Gtk.ButtonBox.ButtonBoxChild w16 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w15 [this.buttonCancel]));
-			w16.Expand = false;
-			w16.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w17 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w16 [this.buttonCancel]));
+			w17.Expand = false;
+			w17.Fill = false;
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonOk = new global::Gtk.Button ();
 			this.buttonOk.CanDefault = true;
@@ -187,14 +197,14 @@ namespace ASCIIArt
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = "gtk-ok";
 			this.AddActionWidget (this.buttonOk, -5);
-			global::Gtk.ButtonBox.ButtonBoxChild w17 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w15 [this.buttonOk]));
-			w17.Position = 1;
-			w17.Expand = false;
-			w17.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w18 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w16 [this.buttonOk]));
+			w18.Position = 1;
+			w18.Expand = false;
+			w18.Fill = false;
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
-			this.DefaultWidth = 461;
+			this.DefaultWidth = 311;
 			this.DefaultHeight = 300;
 			this.Show ();
 		}
